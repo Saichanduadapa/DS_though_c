5cbdc3fb178a7546b3c96b74750c0c4c508880dc
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-12-27 22:05:37 +0530

    Add files via upload

--- binary search using recurtion.cpp
+++ binary search using recurtion.cpp
@@ -17,7 +17,7 @@ false-->
 if(arr[mid]>sc)--->
 if(arr[mid]<sc)--->
 
-*/
+
 
 #include<stdio.h>
 int binary_search(int *arr,int l,int h,int sc)
@@ -54,4 +54,42 @@ int main()
  int res=binary_search(arr,0,n-1,sc);
 		printf("%d",res);
 }
-
+*/
+#include<stdio.h>
+int binary_search(int *arr,int l,int h,int key)
+{
+	int mid;
+	if (l<h)
+	{
+		return -1;
+	}
+	mid=(l+h)/2;
+	if (arr[mid]==key)
+	{
+		return mid;
+	}
+	else if(arr[mid]<key)
+	{
+		l=mid-1;
+	}
+	else if(arr[mid>key])
+	{
+		h=mid+1;
+	}
+	else
+	{
+		binary_search(arr,l,h,key);
+	}
+}
+int main()
+{
+	int n,arr[100],i,res,key;
+	scanf("%d",&n);
+	for (i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	scanf("%d",&key);
+	res=binary_search(arr,0,n-1,key);
+	printf("%d",res);
+}
